6428df99ea56d892d18c712ed408ae932228e6fd
C0sM1n <bordeic4@gmail.com>
2023-08-18 22:42:46 +0300

    smaller source code

--- main.c
+++ main.c
@@ -1,3 +1,3 @@
 #include <stdio.h>
 #define _ if(c==
-s[256],p,l[256],t,c;main(int a,char**b){FILE*i=fopen(b[1],"r");if(!i){return 1;}while((c=fgetc(i))-1){if(feof(i))break;_'+')++s[p];_'-')--s[p];_'>')++p;_'<')--p;_',')scanf("%d",&s[p]);_'.')printf("%c",s[p]);_'['){++t;l[t]=l[t-1];}_']'){--t;if(s[p]){fseek(i,l[t],0);}continue;}++l[t];}return 0;}
+s[256],p,l[256],t,c;main(int a,char**b){FILE*i=fopen(b[1],"r");if(!i)return 1;while((c=fgetc(i))-1){if(feof(i))break;_'+')++s[p];_'-')--s[p];_'>')++p;_'<')--p;_',')scanf("%d",&s[p]);_'.')printf("%c",s[p]);_'['){++t;l[t]=l[t-1];}_']'){--t;if(s[p])fseek(i,l[t],0);--l[t];}++l[t];}}
